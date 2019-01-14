535b3d166c9d2ae480a1666c92413da449a9a8a2
22yuto <since2018yuto22@gmail.com>
2019-01-14 07:57:46 +0000

    jquery.add

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
-
 ActiveRecord::Schema.define(version: 2019_01_13_053136) do
-
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,7 +18,6 @@ ActiveRecord::Schema.define(version: 2019_01_13_053136) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-
     t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
@@ -32,13 +27,7 @@ ActiveRecord::Schema.define(version: 2019_01_13_053136) do
     t.string "first_name"
     t.string "last_name_kana"
     t.string "first_name_kana"
-
-
-
-
-
     t.string "password_confirmation"
-
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
