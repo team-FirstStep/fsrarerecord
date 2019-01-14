7bb2bf29b61772b00f5997a213eeb8e4cf91cadf
22yuto <since2018yuto22@gmail.com>
2019-01-14 09:18:07 +0000

    test3

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,8 +16,3 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
-$(document).ready(function () {
-	$('.jquery').on('click', function(){
-		$(this).css('color','red');
-	});
-});
