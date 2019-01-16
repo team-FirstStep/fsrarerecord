0f4cb77d304d5113bbbeb846424e8ea074d39561
katouken <3a07katouken@gmail.com>
2019-01-16 16:35:26 +0900

    admins db add

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :admins
-#   devise_for :users, controllers: {
-#   sessions:      'users/sessions',
-#   passwords:     'users/passwords',
-#   registrations: 'users/registrations'
-# }
+  devise_for :users, controllers: {
+  sessions:      'users/sessions',
+  passwords:     'users/passwords',
+  registrations: 'users/registrations'
+}
 
-#   devise_for :admins, controllers: {
-#   sessions:      'admins/sessions',
-#   passwords:     'admins/passwords',
-#   registrations: 'admins/registrations'
-# }
+  devise_for :admins, controllers: {
+  sessions:      'admins/sessions',
+  passwords:     'admins/passwords',
+  registrations: 'admins/registrations'
+}
   root 'public/products#index'
 
   namespace :admin do
