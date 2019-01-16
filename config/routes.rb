c3a0d1888fe64fff4650ce0fd9bf87b86a7974ca
katouken <3a07katouken@gmail.com>
2019-01-16 16:29:36 +0900

    admins db add

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
   devise_for :admins
-  devise_for :users, controllers: {
-  sessions:      'users/sessions',
-  passwords:     'users/passwords',
-  registrations: 'users/registrations'
-}
+#   devise_for :users, controllers: {
+#   sessions:      'users/sessions',
+#   passwords:     'users/passwords',
+#   registrations: 'users/registrations'
+# }
 
-  devise_for :admins, controllers: {
-  sessions:      'admins/sessions',
-  passwords:     'admins/passwords',
-  registrations: 'admins/registrations'
-}
+#   devise_for :admins, controllers: {
+#   sessions:      'admins/sessions',
+#   passwords:     'admins/passwords',
+#   registrations: 'admins/registrations'
+# }
   root 'public/products#index'
 
   namespace :admin do
@@ -47,8 +47,6 @@ Rails.application.routes.draw do
   scope module: :public do
     resources :orders, only: [:create]
   end
-
-
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
