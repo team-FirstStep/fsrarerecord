2942d0740151037d03749c99512787efc50324d0
shimishimizu <h200l700@gmail.com>
2019-01-16 14:27:46 +0000

    ログイン、ログアウト時でハンバーガメニューの表示を変えました

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_one :address
+   has_many :addresses, dependent: :destroy
+   accepts_nested_attributes_for :addresses
 end
