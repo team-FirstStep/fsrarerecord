2598ec593d7ba615390a33d07921fd79f63634d0
shimishimizu <h200l700@gmail.com>
2019-01-17 11:45:11 +0000

    新規登録時ユーザー情報が保存できるようにしました

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::Base
+	before_action :configure_sign_up_params, if: :devise_controller?
+
 end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -11,13 +11,17 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # POST /resource
   def create
-    super
-    # begin
-      # ActiveRecord::Base.transaction do
+    begin
+      ActiveRecord::Base.transaction do
+        super
         ###新規登録したユーザー（親）が保有する住所（子）を作成する
         resource.addresses.build
+        resource.addresses[0].address = params[:user][:address][:address]
+        resource.addresses[0].zip = params[:user][:address][:zip]
+        resource.addresses[0].phone = params[:user][:address][:phone]
         resource.save!
-      # end
+      end
+    end
   end
 
 
@@ -45,12 +49,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, addresses_attributes: [:id, :address, :phone, :zip]])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
