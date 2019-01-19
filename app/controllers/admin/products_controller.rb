43ea9dd81e6d06ce709bf1b7dd731649852979c1
katouken <3a07katouken@gmail.com>
2019-01-19 14:02:50 +0900

    katou

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -41,7 +41,7 @@ class Admin::ProductsController < Admin::ApplicationController
   #   render text: "検索結果　#{cnt}件ヒットしました。"
 private
     def product_params
-        params.require(:product).permit(:artists, :cd_title, :jacket_img, :price, :label, :genre, :stock, :value)
+        params.require(:product).permit(:image_id,:artists, :cd_title, :price, :label, :genre, :stock, discs_attributes: [:value])
     end
 
     end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,5 @@ class Product < ApplicationRecord
 
 has_many :selects
 has_many :discs
-accepts_nested_attributes_for :discs
-
+accepts_nested_attributes_for :discs, allow_destroy: true
 end
