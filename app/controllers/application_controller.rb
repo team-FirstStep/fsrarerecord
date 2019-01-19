cfa054b35a9ca7ea41870614ec68c3b436f7c1f3
shimishimizu <h200l700@gmail.com>
2019-01-19 08:15:09 +0000

    ログアウト時のエラー解消

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
 	before_action :configure_sign_up_params, if: :devise_controller?
 
+
+  protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, addresses_attributes: [:id, :address, :phone, :zip]])
+  end
 end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -49,12 +49,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, addresses_attributes: [:id, :address, :phone, :zip]])
-  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
