1a750fabf3b78f54178f0c2c13e563b2ce12f190
shimishimizu <h200l700@gmail.com>
2019-01-20 10:08:17 +0000

    テスト

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -4,21 +4,24 @@ class Public::UsersController < Public::ApplicationController
   def show
     @user = User.find(params[:id])
     @addresses = @user.addresses
-
-
   end
 
   def edit
+    @user = User.find(params[:id])
+    # @addresses = @user.addresses
   end
 
-  def updata
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(current_user)
   end
 
 private
 	def user_params
 		params.require(:user).permit(:last_name, :first_name, :last_name_kana,
 			:first_name_kana, :email, :profile_image,
-			addresses_attributes: [:address, :phone, :zip])
+			addresses_attributes: [:id, :address, :phone, :zip, :_destroy])
 	end
 
 end
