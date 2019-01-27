dd399d996914491a24426ae9b9ea6ce13b15c8b0
shimishimizu <h200l700@gmail.com>
2019-01-27 06:37:10 +0000

    注文履歴表示途中

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -3,6 +3,7 @@ class Public::UsersController < Public::ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @order = Order.where(:cart_id)
     # @address = Address.find_by(user_id: @user.id)
 
   end
