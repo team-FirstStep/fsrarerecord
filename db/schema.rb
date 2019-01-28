2670155c20e89af700b04007aa4c3e67c0286ba8
22yuto <since2018yuto22@gmail.com>
2019-01-28 06:49:22 +0000

    a

--- db/schema.rb
+++ db/schema.rb
@@ -134,8 +134,6 @@ ActiveRecord::Schema.define(version: 2019_01_24_071121) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "deleted_at"
-    t.string "zip"
-    t.string "address"
     t.string "phone"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
