7b7b29b89cf8b6dca12d1f7ea41a615121b15c36
katouken <3a07katouken@gmail.com>
2019-01-28 14:16:05 +0900

    histery 90%

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,6 @@
 class Order < ApplicationRecord
-   has_one :carts
+   belongs_to :cart
+#carts : orders = 1 : 1 だがordersが外部キー(cart_id)を持つので
+#cartに対してorderは子(参照される側)になる！？よってbelong_to
+#　1 : 1でも一応親子関係はある！！
 end
